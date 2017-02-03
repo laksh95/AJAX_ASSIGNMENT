faaf24acdbc8e93e35879fb75a67041bfcce723a
laksh95 <goel.laksh95@gmail.com>
2017-02-03 15:17:53 +0530

    Search by country and language on enter key

--- js/index.js
+++ js/index.js
@@ -1,84 +1,114 @@
 $(function() 
 {
+	var data=[];
+	var company;
 	$('#pagination-demo').twbsPagination(
 	{
 		totalPages: 584,
         visiblePages: 10,
         onPageClick: function (event, page)
         {
-        	var company=$("#search_company").val();
-        	 if(company.trim()!=""){
-				$.ajax(
+        	company=$("#search_company").val();
+			$.ajax(
+			{
+				url:"https://api.github.com/search/repositories?q="+company+"&page="+page+"&per_page=10",
+				success:function(response)
 				{
-					url:"https://api.github.com/search/repositories?q="+company+"&page="+page+"&per_page=10",
+					document.getElementById("list").innerHTML= "";
+					data=response.items;
+					$.each(data, function(index,value)
+					{
+						var list=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+
+						`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+`<h6 class="content-desc">`+data[index].language+' | '+data[index].description+`</h6></li></div>`;
+						$("#list").append(list);
+					});						 
+				},
+				error: function(request,status,error){
+						console.log(error+"error message");
+					}
+			});
+		}
+	});
+	$('#search_name').keypress(function(event)
+	 {
+	 	var flag=0;
+	 	var search=$('#search_name').val();
+	 	console.log(search);
+	 	var slist;
+		if(event.which==13)
+		{
+			if(""==search.trim())
+			{
+				document.getElementById("list").innerHTML= "";
+				$.each(data, function(index,value)
+					{
+						var list=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+
+						`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+`<h6 class="content-desc">`+data[index].language+' | '+data[index].description+`</h6></li></div>`;
+						$("#list").append(list);
+					});
+			}
+			else
+			{
+				document.getElementById("list").innerHTML="";
+				$.each(data, function(index,value)
+				{
+					if(search==data[index].language)
+					{
+						flag=1;
+						slist=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+								
+						`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+data[index].language+`</li></div>`;
+						$("#list").append(slist);
+					}
+				});
+				if(!flag)
+				{
+					document.getElementById("error").innerHTML="No Search Results";
+					// document.getElementById("list").value="";
+				}
+			}
+			
+		}	
+	});
+	$('#search_company').keypress(function(event)
+	 {
+	 	company=$('#search_company').val();
+	 	var slist;
+		if(event.which==13)
+		{
+			$.ajax(
+				{
+					url:"https://api.github.com/search/repositories?q="+company+"&page=1&per_page=10",
 					success:function(response)
 					{
 						document.getElementById("list").innerHTML= "";
 						console.log("Pagination");
-						var data=response.items;
+						data=response.items;
 						$.each(data, function(index,value)
 						{
 							var list=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+
 							`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+`<h6 class="content-desc">`+data[index].language+' | '+data[index].description+`</h6></li></div>`;
 							$("#list").append(list);
-						});
-						 $('#search_name').keypress(function(event)
-						 {
-						 	var search=$('#search_name').val();
-						 	console.log(search);
-						 	var slist;
-							if(event.which==13)
-							{
-								$('#on_load').hide();
-								document.getElementById("search_list").innerHTML="";
-								$.each(data, function(index,value)
-								{
-									if(search==data[index].language)
-									{
-										slist=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+								
-										`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+data[index].language+`</li></div>`;
-										$("#search_list").append(slist);
-									}
-								});
-							}
-						});
-						 $('#search_company').keypress(function(event)
-						 {
-						 	var search=$('#search_name').val();
-						 	console.log(search);
-						 	var slist;
-							if(event.which==13)
-							{
-								$('#on_load').hide();
-								document.getElementById("search_list").innerHTML="";
-								$.each(data, function(index,value)
-								{
-									if(search==data[index].language)
-									{
-										slist=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+								
-										`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+data[index].language+`</li></div>`;
-										$("#search_list").append(slist);
-									}
-								});
-							}
-						});
-						 $("[type=range]").change(function(){
-	    					var newval=$(this).val();
-	    					document.getElementById("search_list").innerHTML="";
-	    					document.getElementById("value").innerHTML=newval;
-	    					$.each(data, function(index,value)
-							{
-								if(data[index].stargazers_count<=newval)
-								{
-									slist=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+								
-									`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+data[index].language+`</li></div>`;
-									$("#search_list").append(slist);
-								}
-							});
-	  					});
-					}
+						});						 
+					},
+					error: function(request,status,error){
+							console.log(error);
+						}
 				});
-			}
+			
 		}
+	});
+	$("[type=range]").change(function(){
+		var newval=$(this).val();
+		document.getElementById("search_list").innerHTML="";
+		document.getElementById("value").innerHTML=newval;
+		$.each(data, function(index,value)
+		{
+			if(data[index].stargazers_count<=newval)
+			{
+				slist=`<div class="result"><li class="media" style="list-style:none; padding:5px;"><img src="img/head.jpg" class="d-flex mr-3"  style="float:left;width:64px;height:64px;">`+								
+				`<div class="media-body"><h5 class="content">`+data[index].name+`</h5><br /></div>`+data[index].language+`</li></div>`;
+				$("#search_list").append(slist);
+			}
+		});
 	});
 });
